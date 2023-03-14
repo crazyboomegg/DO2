797b10c7e9b13d45e6cfece616939f0ec53cbc01
crazyboomegg <crazyboomegg@gmail.com>
2023-03-14 22:35:21 +0800

    first model

---
+++ DO2/DO2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DO2
+//
+//  Created by 江柏毅 on 2023/3/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
