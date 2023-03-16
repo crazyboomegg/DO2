2868ff538d2b2ba723737ce2a93bf02ea4e7b775
wyn <k022298@gmail.com>
2023-03-16 17:15:51 +0000

    Defined Google map view in how page

--- DO2/DO2/Application/AppDelegate.swift
+++ DO2/DO2/Application/AppDelegate.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    // Replace your API key here
+    let apiKey: String = <#Enter your API key here#>
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey(apiKey)
         return true
     }
 
