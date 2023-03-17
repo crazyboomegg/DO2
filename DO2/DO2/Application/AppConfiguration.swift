9445b8203de59e751aab47487da32a02628926e1
wyn <k022298@gmail.com>
2023-03-17 15:53:34 +0000

    Defined AppConfiguration and googleMapAPI

---
+++ DO2/DO2/Application/AppConfiguration.swift
@@ -0,0 +1,12 @@
+//
+//  AppConfiguration.swift
+//  DO2
+//
+//  Created by wyn on 2023/3/17.
+//
+
+final class AppConfiguration {
+    private init() {}
+    // Replace your API key here
+    static let googleMapAPI: String = <#Enter your API key here#>
+}
